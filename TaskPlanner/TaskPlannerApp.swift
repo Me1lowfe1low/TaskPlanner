9aa4438ad1bc642c67b81637afd50c2863eefb01
C'est une pomme <dmgordienko@gmail.com>
2023-01-30 17:31:03 +0400

    Initial Commit

---
+++ TaskPlanner/TaskPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskPlannerApp.swift
+//  TaskPlanner
+//
+//  Created by Дмитрий Гордиенко on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
